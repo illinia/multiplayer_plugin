2d519b1d1efe8d918f441c44d841c2a5b6e783f2
TAEMIN <seolin1218@gmail.com>
2024-11-07 22:58:31 +0900

    Accessing our Subsystem

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Menu.h"
+#include "MultiplayerSessionsSubsystem.h"
+#include "Components/Button.h"
 
 void UMenu::MenuSetup()
 {
@@ -23,5 +25,50 @@ void UMenu::MenuSetup()
 			PlayerController->SetShowMouseCursor(true);
 			
 		}
+	}
+
+	UGameInstance* GameInstance = GetGameInstance();
+	if (GameInstance)
+	{
+		MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+	}
+}
+
+bool UMenu::Initialize()
+{
+	if (!Super::Initialize())
+	{
+		return false;
+	}
+
+	if (HostButton)
+	{
+		HostButton->OnClicked.AddDynamic(this, &ThisClass::UMenu::HostButtonClicked);
+	}
+	if (JoinButton)
+	{
+		JoinButton->OnClicked.AddDynamic(this, &ThisClass::UMenu::JoinButtonClicked);
+	}
+	
+	return true;
+}
+
+void UMenu::HostButtonClicked()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString(TEXT("Host Button Clicked")));
+	}
+	if (MultiplayerSessionsSubsystem)
+	{
+		MultiplayerSessionsSubsystem->CreateSession(4, FString("FreeForAll"));
+	}
+}
+
+void UMenu::JoinButtonClicked()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Yellow, FString(TEXT("Join Button Clicked")));
 	}
 }
